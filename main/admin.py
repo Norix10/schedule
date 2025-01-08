59a0be3cf5e9f5531815533cc32fb4cad03f7505
Norix10 <norix123321@gmail.com>
2025-01-08 23:57:47 +0200

    teacher table

---
+++ main/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Lesson, Teacher
+# from .models import Teacher
+
+admin.site.register(Lesson)
+admin.site.register(Teacher)
+
---
+++ main/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+class Lesson(models.Model):
+    day_of_week = models.CharField(max_length=10, choices=[
+        ('Monday', 'Понеділок'),
+        ('Tuesday', 'Вівторок'),
+        ('Wednesday', 'Середа'),
+        ('Thursday', 'Четвер'),
+        ('Friday', 'П’ятниця'),
+        ('Saturday', 'Субота'),
+    ])
+    time = models.TimeField()
+    subject = models.CharField(max_length=100)
+    teacher = models.CharField(max_length=100)
+    room = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.subject} ({self.day_of_week}, {self.time})"
+
+
+class Teacher(models.Model):
+    first_name = models.CharField(max_length=100)
+    middle_name = models.CharField(max_length=100, default='')
+    last_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.first_name} {self.middle_name} {self.last_name}"
